5ad27745fc350f12b3a58edb794af93f5a3f1b56
Henrique Uchida <90984644+HenriqueUchida@users.noreply.github.com>
2025-09-30 02:37:09 +0000

    iniciando as funçoes dos botoes

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,105 @@
 let conteudoPlacar1 = document.querySelector('h2#container-placar-1')
 let conteudoPlacar2 = document.querySelector('h2#container-placar-2')
-const somaVermelha1 = document.getElementById('soma-vermelha-1')
-console.log(conteudoPlacar1)
+let somaVermelha1 = document.getElementById('soma-vermelha-1') 
+let somaAmarela1 = document.getElementById('soma-vermelha-1')
+let somaVerde1 = document.getElementById('soma-vermelha-1')
+let somaMarrom1 = document.getElementById('soma-vermelha-1')
+let somaAzul1 = document.getElementById('soma-vermelha-1')
+let somaRosa1 = document.getElementById('soma-vermelha-1')
+let somaPreto1 = document.getElementById('soma-vermelha-1')
+
+let subVermelha1 = document.getElementById('soma-vermelha-1') 
+let subAmarela1 = document.getElementById('soma-vermelha-1') 
+let subVerde1 = document.getElementById('soma-vermelha-1') 
+let subMarrom1 = document.getElementById('soma-vermelha-1') 
+let subAzul1 = document.getElementById('soma-vermelha-1') 
+let subRosa1 = document.getElementById('soma-vermelha-1') 
+let subPreto1 = document.getElementById('soma-vermelha-1') 
+
+let somaVermelha2 = document.getElementById('soma-vermelha-1')
+let somaAmarela2 = document.getElementById('soma-vermelha-1')
+let somaVerde2 = document.getElementById('soma-vermelha-1')
+let somaMarrom2 = document.getElementById('soma-vermelha-1')
+let somaAzul2 = document.getElementById('soma-vermelha-1')
+let somaRosa2 = document.getElementById('soma-vermelha-1')
+let somaPreto2 = document.getElementById('soma-vermelha-1')
+
+let subVermelha2 = document.getElementById('soma-vermelha-1') 
+let subAmarela2 = document.getElementById('soma-vermelha-1') 
+let subVerde2 = document.getElementById('soma-vermelha-1') 
+let subMarrom2 = document.getElementById('soma-vermelha-1') 
+let subAzul2 = document.getElementById('soma-vermelha-1') 
+let subRosa2 = document.getElementById('soma-vermelha-1') 
+let subPreto2 = document.getElementById('soma-vermelha-1') 
+
+somaVermelha1 = 1
+somaAmarela1 = 2 
+somaVerde1 = 3 
+somaMarrom1 = 4
+somaAzul1 = 5
+somaRosa1 = 6
+somaPreto1 = 7
+
+subVermelha1 = -1
+subAmarela1 = -2
+subVerde1 = -3 
+subMarrom1 = -4
+subAzul1 = -5
+subRosa1 = -6
+subPreto1 = -7
+
+somaVermelha2 = 1
+somaAmarela2 = 2 
+somaVerde2 = 3 
+somaMarrom2 = 4
+somaAzul2 = 5
+somaRosa2 = 6
+somaPreto2 = 7
+
+subVermelha2 = -1
+subAmarela2 = -2
+subVerde2 = -3 
+subMarrom2 = -4
+subAzul2 = -5
+subRosa2 = -6
+subPreto2 = -7
+
+somaVermelha1.addEventListener('click', function() {
+    console.log(somaVermelha1)
+    // conteudoPlacar1.innerHTML = `<h2>${Number(conteudoPlacar1.textContent) + 1}</h2>`
+})
+somaAmarela1.addEventListener('click', somaPlacar())
+somaVerde1.addEventListener('click', somaPlacar())
+somaMarrom1.addEventListener('click', somaPlacar())
+somaAzul1.addEventListener('click', somaPlacar())
+somaRosa1.addEventListener('click', somaPlacar())
+somaPreto1.addEventListener('click', somaPlacar())
+													 
+subVermelha1.addEventListener('click', somaPlacar())
+subAmarela1.addEventListener('click', somaPlacar())
+subVerde1.addEventListener('click', somaPlacar())
+subMarrom1.addEventListener('click', somaPlacar())
+subAzul1.addEventListener('click', somaPlacar())
+subRosa1.addEventListener('click', somaPlacar())
+subPreto1.addEventListener('click', somaPlacar())
+													 
+somaVermelha2.addEventListener('click', somaPlacar())
+somaAmarela2.addEventListener('click', somaPlacar())
+somaVerde2.addEventListener('click', somaPlacar())
+somaMarrom2.addEventListener('click', somaPlacar())
+somaAzul2.addEventListener('click', somaPlacar())
+somaRosa2.addEventListener('click', somaPlacar())
+somaPreto2.addEventListener('click', somaPlacar())
+													 
+subVermelha2.addEventListener('click', somaPlacar())
+subAmarela2.addEventListener('click', somaPlacar())
+subVerde2.addEventListener('click', somaPlacar())
+subMarrom2.addEventListener('click', somaPlacar())
+subAzul2.addEventListener('click', somaPlacar())
+subRosa2.addEventListener('click', somaPlacar())
+subPreto2.addEventListener('click', somaPlacar())
+
+function somaPlacar () {
+    
+}
+
