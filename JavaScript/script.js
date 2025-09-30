8a4318b579f9e77b4456970b6b84092199063f99
Henrique Uchida <90984644+HenriqueUchida@users.noreply.github.com>
2025-09-30 18:25:47 +0000

    corrigindo script

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,71 +1,41 @@
-let conteudoPlacar1 = document.querySelector('h2#container-placar-1')
-let conteudoPlacar2 = document.querySelector('h2#container-placar-2')
+let conteudoPlacar1 = document.querySelector('h2#conteudo-placar-1')
+let conteudoPlacar2 = document.querySelector('h2#conteudo-placar-2')
 let somaVermelha1 = document.getElementById('soma-vermelha-1') 
-let somaAmarela1 = document.getElementById('soma-vermelha-1')
-let somaVerde1 = document.getElementById('soma-vermelha-1')
-let somaMarrom1 = document.getElementById('soma-vermelha-1')
-let somaAzul1 = document.getElementById('soma-vermelha-1')
-let somaRosa1 = document.getElementById('soma-vermelha-1')
-let somaPreto1 = document.getElementById('soma-vermelha-1')
+let somaAmarela1 = document.getElementById('soma-amarela-1')
+let somaVerde1 = document.getElementById('soma-verde-1')
+let somaMarrom1 = document.getElementById('soma-marrom-1')
+let somaAzul1 = document.getElementById('soma-azul-1')
+let somaRosa1 = document.getElementById('soma-rosa-1')
+let somaPreto1 = document.getElementById('soma-preto-1')
 
-let subVermelha1 = document.getElementById('soma-vermelha-1') 
-let subAmarela1 = document.getElementById('soma-vermelha-1') 
-let subVerde1 = document.getElementById('soma-vermelha-1') 
-let subMarrom1 = document.getElementById('soma-vermelha-1') 
-let subAzul1 = document.getElementById('soma-vermelha-1') 
-let subRosa1 = document.getElementById('soma-vermelha-1') 
-let subPreto1 = document.getElementById('soma-vermelha-1') 
+let subVermelha1 = document.getElementById('sub-vermelha-1') 
+let subAmarela1 = document.getElementById('sub-amarela-1') 
+let subVerde1 = document.getElementById('sub-verde-1') 
+let subMarrom1 = document.getElementById('sub-marrom-1') 
+let subAzul1 = document.getElementById('sub-azul-1') 
+let subRosa1 = document.getElementById('sub-rosa-1') 
+let subPreto1 = document.getElementById('sub-preto-1') 
 
-let somaVermelha2 = document.getElementById('soma-vermelha-1')
-let somaAmarela2 = document.getElementById('soma-vermelha-1')
-let somaVerde2 = document.getElementById('soma-vermelha-1')
-let somaMarrom2 = document.getElementById('soma-vermelha-1')
-let somaAzul2 = document.getElementById('soma-vermelha-1')
-let somaRosa2 = document.getElementById('soma-vermelha-1')
-let somaPreto2 = document.getElementById('soma-vermelha-1')
+let somaVermelha2 = document.getElementById('soma-vermelha-2')
+let somaAmarela2 = document.getElementById('soma-amarela-2')
+let somaVerde2 = document.getElementById('soma-verde-2')
+let somaMarrom2 = document.getElementById('soma-marrom-2')
+let somaAzul2 = document.getElementById('soma-azul-2')
+let somaRosa2 = document.getElementById('soma-rosa-2')
+let somaPreto2 = document.getElementById('soma-preto-2')
 
-let subVermelha2 = document.getElementById('soma-vermelha-1') 
-let subAmarela2 = document.getElementById('soma-vermelha-1') 
-let subVerde2 = document.getElementById('soma-vermelha-1') 
-let subMarrom2 = document.getElementById('soma-vermelha-1') 
-let subAzul2 = document.getElementById('soma-vermelha-1') 
-let subRosa2 = document.getElementById('soma-vermelha-1') 
-let subPreto2 = document.getElementById('soma-vermelha-1') 
+let subVermelha2 = document.getElementById('sub-vermelha-2') 
+let subAmarela2 = document.getElementById('sub-amarela-2') 
+let subVerde2 = document.getElementById('sub-verde-2') 
+let subMarrom2 = document.getElementById('sub-marrom-2') 
+let subAzul2 = document.getElementById('sub-azul-2') 
+let subRosa2 = document.getElementById('sub-rosa-2') 
+let subPreto2 = document.getElementById('sub-preto-2') 
 
-somaVermelha1 = 1
-somaAmarela1 = 2 
-somaVerde1 = 3 
-somaMarrom1 = 4
-somaAzul1 = 5
-somaRosa1 = 6
-somaPreto1 = 7
-
-subVermelha1 = -1
-subAmarela1 = -2
-subVerde1 = -3 
-subMarrom1 = -4
-subAzul1 = -5
-subRosa1 = -6
-subPreto1 = -7
-
-somaVermelha2 = 1
-somaAmarela2 = 2 
-somaVerde2 = 3 
-somaMarrom2 = 4
-somaAzul2 = 5
-somaRosa2 = 6
-somaPreto2 = 7
-
-subVermelha2 = -1
-subAmarela2 = -2
-subVerde2 = -3 
-subMarrom2 = -4
-subAzul2 = -5
-subRosa2 = -6
-subPreto2 = -7
 
 somaVermelha1.addEventListener('click', function() {
-    console.log(somaVermelha1)
+    // conteudoPlacar1.textContent = 0
+    console.log(conteudoPlacar1.textContent)
     // conteudoPlacar1.innerHTML = `<h2>${Number(conteudoPlacar1.textContent) + 1}</h2>`
 })
 somaAmarela1.addEventListener('click', somaPlacar())
