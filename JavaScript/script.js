bb5797dce4b7da3676959b521dc49e96566187f2
Henrique Uchida <90984644+HenriqueUchida@users.noreply.github.com>
2025-09-30 02:06:11 +0000

    iniciando o placar snooker

---
+++ JavaScript/script.js
@@ -0,0 +1,4 @@
+let conteudoPlacar1 = document.querySelector('h2#container-placar-1')
+let conteudoPlacar2 = document.querySelector('h2#container-placar-2')
+const somaVermelha1 = document.getElementById('soma-vermelha-1')
+console.log(conteudoPlacar1)
