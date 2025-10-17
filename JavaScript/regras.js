e7764289f0564fe1d8f21e4c6ed2484645881a9b
Henrique Uchida <90984644+HenriqueUchida@users.noreply.github.com>
2025-10-17 03:12:20 +0000

    adicionando resumo ao final da partida

--- JavaScript/regras.js
+++ JavaScript/regras.js
@@ -4,7 +4,10 @@ let conteudoPlacar2 = document.querySelector('#conteudo-placar-2');
 let empates = 0;
 let vitoriasJogador1 = document.querySelector('#qtde-vitorias-1');
 let vitoriasJogador2 = document.querySelector('#qtde-vitorias-2');
-let partidasJogadadas = []
+let partidasJogadadas = [];
+let nomejogador1 = document.querySelector('#descricao-jogador-1');
+let nomejogador2 = document.querySelector('#descricao-jogador-2');
+let overlay = document.getElementById('overlay');
 function criaEventos(){
     const containerMain = document.querySelector('#placar-da-partida');
     containerMain.addEventListener('click', (event) => {
@@ -15,17 +18,19 @@ function criaEventos(){
             // console.log(jogador, valorBotao);
             atualizaPlacar(jogador, valorBotao);
         } else if (event.target.matches('.reinicar')) {
-            const numConteudoPlacar1 = Number(conteudoPlacar1.textContent)
-            const numConteudoPlacar2 = Number(conteudoPlacar2.textContent)
+            const numConteudoPlacar1 = Number(conteudoPlacar1.textContent);
+            const numConteudoPlacar2 = Number(conteudoPlacar2.textContent);
             // console.log(numConteudoPlacar1, numConteudoPlacar2)
-            reiniciaPartida(numConteudoPlacar1, numConteudoPlacar2);
+            verficaGanhador(numConteudoPlacar1, numConteudoPlacar2);
+            reiniciaPartida();
         } else if (event.target.matches('.finalizar')){
-            const numConteudoPlacar1 = Number(conteudoPlacar1.textContent)
-            const numConteudoPlacar2 = Number(conteudoPlacar2.textContent)
-            finalizarJogo(numConteudoPlacar1, numConteudoPlacar2);
+            const numConteudoPlacar1 = Number(conteudoPlacar1.textContent);
+            const numConteudoPlacar2 = Number(conteudoPlacar2.textContent);
+            verficaGanhador(numConteudoPlacar1, numConteudoPlacar2);
+            finalizarJogo();
         } else if (event.target.matches('.edit-jogador')){
             const jogadorEdit = event.target;
-            const jogador = jogadorEdit.dataset.jogador;
+            const jogador = Number(jogadorEdit.dataset.jogador);
             editarJogador(jogador)
         }
         // console.log(event.target)
@@ -42,72 +47,93 @@ function atualizaPlacar(jogador, valorBolinha){
     }
 };
 
-function reiniciaPartida(numConteudoPlacar1, numConteudoPlacar2){
-    console.log(partidasJogadadas);
-    if (numConteudoPlacar1 == 0 && numConteudoPlacar2 == 0){
+function reiniciaPartida(){
+    conteudoPlacar1.textContent = 0;
+    conteudoPlacar2.textContent = 0;
+};
+
+function finalizarJogo(){
+     if(partidasJogadadas.length > 0){
+        // const body = document.querySelector('body');
+        const containerMain = document.querySelector('#placar-da-partida');
+        /*Exemplo para pegar o ultimo elemento do array*/
+        // const ultimaPartida = historicoPartidas[historicoPartidas.length - 1];    
+        // // 3. Pega o valor da propriedade 'id_partida' desse objeto
+        // const totalPartidas = ultimaPartida.id_partida;
+        
+        const partidas = partidasJogadadas[partidasJogadadas.length - 1];
+        const ultimaPartida = partidas.partida;
+        const janelaResumo = document.createElement('div');
+        const cabecalhoResumo = document.createElement('div');
+        const conteudoResumo = document.createElement('div');
+        const mensagemResumo = document.createElement('div');
+        const btnFechaResumo = document.createElement('div');
+        janelaResumo.className = 'resumo';
+        cabecalhoResumo.className = 'cabecalho-resumo';
+        conteudoResumo.className = 'conteudo-resumo';
+        mensagemResumo.className = 'mensagem-resumo';
+        btnFechaResumo.className = 'container-fechar-resumo'
+        cabecalhoResumo.innerHTML = '<h3>Resumo do Jogo</h3>';
+        conteudoResumo.innerHTML += `<p>Foram jogadas: ${ultimaPartida} partidas</p>`;
+        conteudoResumo.innerHTML += `<p>${jogador1.textContent} venceu: ${vitoriasJogador1.textContent} partidas</p>`;
+        conteudoResumo.innerHTML += `<p>${jogador2.textContent} venceu: ${vitoriasJogador2.textContent} partidas</p>`;
+        conteudoResumo.innerHTML += `<p>Empates: ${empates}</p>`;
+        mensagemResumo.innerHTML = `<div><p>Tire seu print! &#128512;</p><div>`;
+        btnFechaResumo.innerHTML = `<button id="fechar-resumo">Continuar</button>`;
+        janelaResumo.appendChild(cabecalhoResumo,conteudoResumo,mensagemResumo,btnFechaResumo);
+        overlay.style.display = 'block'
+        containerMain.appendChild(janelaResumo);
+        const fecharResumo = document.getElementById('fechar-resumo');
+        fecharResumo.addEventListener('click', () => {
+            window.location.reload();
+        })
+     } else {
+        alert('Nenhuma partida foi jogada!');
+     }
+};
+
+function verficaGanhador(placarJogador1, placarJogador2){
+    if (placarJogador1 == 0 && placarJogador2 == 0){
+        console.log('Jogo não iniciado');
         return
     }
-    if (numConteudoPlacar1 == numConteudoPlacar2) {
-        partidasJogadadas.push({partida: `${qtdePartidas.textContent}`, pontuacaoJogador1: `${numConteudoPlacar1}`, pontuacaoJogador2: `${numConteudoPlacar2}`});
-        console.log(numConteudoPlacar1, numConteudoPlacar2);
+    if (placarJogador1 == placarJogador2) {
+        partidasJogadadas.push({partida: `${qtdePartidas.textContent}`, nomeJogador1: `${jogador1.textContent}`, pontuacaoJogador1: `${placarJogador1}`, nomeJogador2: `${jogador2.textContent}`, pontuacaoJogador2: `${placarJogador2}`});
         empates += 1;
         qtdePartidas.textContent = Number(qtdePartidas.textContent) + 1;
-        console.log('empatou');
-        conteudoPlacar1.textContent = 0;
-        conteudoPlacar2.textContent = 0;
-    } else if (numConteudoPlacar1 > numConteudoPlacar2) {
-        partidasJogadadas.push({partida: `${qtdePartidas.textContent}`, pontuacaoJogador1: `${numConteudoPlacar1}`, pontuacaoJogador2: `${numConteudoPlacar2}`});
-        console.log(numConteudoPlacar1, numConteudoPlacar2);
+
+    } else if (placarJogador1 > placarJogador2) {
+        partidasJogadadas.push({partida: `${qtdePartidas.textContent}`, nomeJogador1: `${jogador1.textContent}`, pontuacaoJogador1: `${placarJogador1}`, nomeJogador2: `${jogador2.textContent}`, pontuacaoJogador2: `${placarJogador2}`});
         qtdePartidas.textContent = Number(qtdePartidas.textContent) + 1;
         vitoriasJogador1.textContent = Number(vitoriasJogador1.textContent) + 1;
-        console.log(`jogador 1 ganhou`);
-        conteudoPlacar1.textContent = 0;
-        conteudoPlacar2.textContent = 0;
 
-    } else if (numConteudoPlacar1 < numConteudoPlacar2){
-        partidasJogadadas.push({partida: `${qtdePartidas.textContent}`, pontuacaoJogador1: `${numConteudoPlacar1}`, pontuacaoJogador2: `${numConteudoPlacar2}`});
-        console.log(numConteudoPlacar1, numConteudoPlacar2);
+    } else if (placarJogador1 < placarJogador2){
+        partidasJogadadas.push({partida: `${qtdePartidas.textContent}`, nomeJogador1: `${jogador1.textContent}`, pontuacaoJogador1: `${placarJogador1}`, nomeJogador2: `${jogador2.textContent}`, pontuacaoJogador2: `${placarJogador2}`});
         qtdePartidas.textContent = Number(qtdePartidas.textContent) + 1;
         vitoriasJogador2.textContent = Number(vitoriasJogador2.textContent) + 1;
-        console.log(`jogador 2 ganhou`);
-        conteudoPlacar1.textContent = 0;
-        conteudoPlacar2.textContent = 0;
     }
-    console.log('após reinicio');
-    console.log(partidasJogadadas);
-};
-
-function finalizarJogo(numConteudoPlacar1, numConteudoPlacar2){
-    if (numConteudoPlacar1 == 0 && numConteudoPlacar1 == 0){
-        const ultimaPartida = partidasJogadadas.at(-1).partida;
-        // console.log(ultimaPartida);
-        return
-    }
-    if (numConteudoPlacar1 == numConteudoPlacar2 && numConteudoPlacar1 !=0 || numConteudoPlacar2 !=0) {
-        // partidasJogadadas.push({partida: `${qtdePartidas.textContent}`, pontuacaoJogador1: `${numConteudoPlacar1}`, pontuacaoJogador2: `${numConteudoPlacar2}`});
-        // empates += 1;
-        // conteudoPlacar1.textContent = 0;
-        // conteudoPlacar2.textContent = 0;
-    } else if (numConteudoPlacar1 > numConteudoPlacar2) {
-        // partidasJogadadas.push({partida: `${qtdePartidas.textContent}`, pontuacaoJogador1: `${numConteudoPlacar1}`, pontuacaoJogador2: `${numConteudoPlacar2}`});
-        // qtdePartidas.textContent = Number(qtdePartidas.textContent) + 1;
-        // vitoriasJogador1.textContent = Number(vitoriasJogador1.textContent) + 1;
-        // console.log(`jogador 1 ganhou`);
-        // conteudoPlacar1.textContent = 0;
-        // conteudoPlacar2.textContent = 0;
-
-    } else if (numConteudoPlacar1 < numConteudoPlacar2){
-        // partidasJogadadas.push({partida: `${qtdePartidas.textContent}`, pontuacaoJogador1: `${numConteudoPlacar1}`, pontuacaoJogador2: `${numConteudoPlacar2}`});
-        // qtdePartidas.textContent = Number(qtdePartidas.textContent) + 1;
-        // vitoriasJogador2.textContent = Number(vitoriasJogador2.textContent) + 1;
-        // console.log(`jogador 2 ganhou`);
-        // conteudoPlacar1.textContent = 0;
-        // conteudoPlacar2.textContent = 0;
-    }
-};
+}
 
 function editarJogador(jogador){
-    
+    //let nomejogador1 = document.querySelector('#descricao-jogador-1'); colinha pra não voltar no inicio do codigo
+    //let nomejogador2 = document.querySelector('#descricao-jogador-2');
+    const containerMain = document.querySelector('#placar-da-partida');
+    const containerEdicao = document.createElement('div');
+    const digitarNome = document.createElement('input'); //type="text" name="" id="">')
+    const containerComandos = document.createElement('div');
+    const cancelaEdicao = document.createElement('button');
+    const aplicaEdicao = document.createElement('button');
+    let novoNome = nomeJogador`${jogador}`.textContent
+    containerEdicao.className = 'edicao-de-nomes';
+    cancelaEdicao.textContent = 'Cancelar';
+    aplicaEdicao.textContent = 'Aplicar';
+    containerComandos.appendChild(cancelaEdicao, aplicaEdicao);
+    containerEdicao.appendChild(containerComandos);
+    digitarNome.type = 'text';
+    digitarNome.placeholder = 'digite o nome desejado';
+    if (jogador == 1){
+
+    }
 }
 
 document.addEventListener('DOMContentLoaded', criaEventos());
